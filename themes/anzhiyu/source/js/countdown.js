90fd68d5c266b22a5ac3488e452427bcf0a21a53
Amiracle <amiracletm@gmail.com>
2026-04-07 15:08:15 +0800

    chore: update widget info

--- themes/anzhiyu/source/js/countdown.js
+++ themes/anzhiyu/source/js/countdown.js
@@ -1,7 +1,7 @@
 const CountdownTimer = (() => {
     const config = {
-        targetDate: "2026-04-05",
-        targetName: "清明",
+        targetDate: "2026-05-01",
+        targetName: "劳动节",
         units: {
             day: { text: "今日", unit: "小时" },
             week: { text: "本周", unit: "天" },
